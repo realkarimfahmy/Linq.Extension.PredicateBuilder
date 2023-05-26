f598e604055a7b4e9a987879e504d42ff7150e48
Karim Fahmy <35509009+realkarimfahmy@users.noreply.github.com>
2023-05-26 09:54:16 +0300

    Delete src/PredicateBuilder directory

--- src/PredicateBuilder/SearchFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Linq.Extension.PredicateBuilder
-{
-    public class SearchFilter
-    {
-        public string PropertyName { get; set; }
-        public OperatorComparer Operation { get; set; }
-        public object Value { get; set; }
-    }
-}
